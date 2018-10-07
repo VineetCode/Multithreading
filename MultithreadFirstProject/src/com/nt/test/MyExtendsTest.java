2c80c268f36de09ba4776d67c75b8cf965e2c895
Vineet Singh <singhvineet238@gmail.com>
2018-10-07 11:15:41 +0530

    Create MyThread using extends Thread with first Approach
    Change-Id: Id1cf7c17e5dbb1ed216367f4460ade3e802783f1
    Signed-off-by: Vineet Singh <singhvineet238@gmail.com>

---
+++ MultithreadFirstProject/src/com/nt/test/MyExtendsTest.java
@@ -0,0 +1,11 @@
+package com.nt.test;
+
+import com.nt.basic.MyThread;
+
+public class MyExtendsTest {
+	public static void main(String[] args) {
+		MyThread t1=new MyThread();
+		t1.start();
+	}
+
+}
